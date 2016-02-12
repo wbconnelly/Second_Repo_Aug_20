40b81b8aba53f6ba542cdafeccab7e1860a5d40e
Bill Connelly <wbconnelly@gmail.com>
2016-02-12 02:07:51 -0500

    Update ajax_ex.js

--- ajax_ex.js
+++ ajax_ex.js
@@ -1,3 +1,4 @@
+jQuery(function($){
 $(function changestate() {
 
 var $first_row = $('#first_row')
@@ -7,13 +8,15 @@ $.ajax({
 	type: 'GET',
 	url: 'https://opendata.socrata.com/resource/a6x9-fw3a.json?state_name='+ state_filter,
 	success: function(data) {$.each(data, function(i, item) {
-		$first_row.after('<tr id = "table_row"><td>' + item.measure_category +'</td><td>' + item._2010 +'</td><td>' + item._2011 + '</td><td>' + item._2012 + '</td><td>' + item._2013 + '</td><td>' + item._2014 + '</td></tr>' );
+		$first_row.after('<tr class = "table_row"><td>' + item.measure_category +'</td><td>' + item._2010 +'</td><td>' + item._2011 + '</td><td>' + item._2012 + '</td><td>' + item._2013 + '</td><td>' + item._2014 + '</td></tr>' );
 		})
 	console.log(data);
 	}
 });
-//$('#btn3').click(changestate);
+
 }
-)
+)	
+	
+});
 
 

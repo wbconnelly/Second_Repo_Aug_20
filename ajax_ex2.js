eebaab113954582cd7b770c9cd4e804f1b7b4538
Bill Connelly <wbconnelly@gmail.com>
2016-02-12 00:52:31 -0500

    Create ajax_ex2.js

---
+++ ajax_ex2.js
@@ -0,0 +1,16 @@
+$(function replace_table() {
+
+var $table_row = $('#first_row')
+var state_filter = $('#state_select').val()
+
+$.ajax({
+	type: 'GET',
+	url: 'https://opendata.socrata.com/resource/a6x9-fw3a.json?state_name='+ state_filter,
+	success: function(data) {$.each(data, function(i, item) {
+		$table_row.replaceWith('<tr id = "table_row"><td>' + item.measure_category +'</td><td>' + item._2010 +'</td><td>' + item._2011 + '</td><td>' + item._2012 + '</td><td>' + item._2013 + '</td><td>' + item._2014 + '</td></tr>' );
+		})
+	console.log(data);
+	}
+});
+})
+
